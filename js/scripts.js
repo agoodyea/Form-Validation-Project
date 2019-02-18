4da56809cbf726fab4804ea8eb70efcc58ca4518
Aiden Goodyear <agoodyea30@gmail.com>
2019-02-18 12:59:31 +0800

    added two main features
    Added the activity clash feature and the payment message display feature.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,15 @@ Treehouse Techdegree:
 FSJS project 3 - Interactive form
 ******************************************/
 
+let activityTotal = 0;
+
 // initialisation code
 $('#name').focus();
 $('#other-title').hide();
+$('.activities').append($('span'));
+$('.activities span').css('color', 'green');
+$('.activities span').hide();
+
 
 // job title event listener
 $('#title').on('change', function(e){
@@ -16,7 +22,8 @@ $('#title').on('change', function(e){
     }
 })
 
-// t-shirt selection event listner
+// T-SHIRT SELECTION SECTION ----------------------------------------------
+
 $('#design').on('change', function(e){
     if($(e.target).val() === 'js puns'){
         $('#color option:eq(0)').attr('selected', true);
@@ -48,21 +55,102 @@ $('#design').on('change', function(e){
     }
 });
 
-// activities
+// ACTIVITIES SECTION -------------------------------------------------------
+
+// track and display total cost
 $('.activities').on('change', function(e){
-    total
+    let $activityText = $(e.target).parent().text();
+    let strEnd = $activityText.length;
+    let cost = parseInt($activityText.slice(-3, strEnd));
+    $('.activities span').show();
+
+    if ($(event.target).is(':checked')) {
+        activityTotal += cost;
+        $('.activities span').text('$' + activityTotal);
+    }else{
+        activityTotal -= cost;
+        $('.activities span').text('$' + activityTotal);
+    }
+
+    if (activityTotal === 0) {
+        $('.activities span').hide();
+    }
 })
 
-/*
-    -When an activity is checked, see if there is another activity
-     with a conflicting time and disable it.
-    -When an activity is unchecked, see if there is a confilicting time
-     that can be undisabled. 
-    -Add the price of checked activity to the total price.
-    -Remove price of unchecked activity from total price. 
+// variables and function for activity clashes
+const $jsFrameWorks = $('.activities label input:eq(1)');
+const $jsLibs = $('.activities label input:eq(2)');
+const $express = $('.activities label input:eq(3)');
+const $nodeJs = $('.activities label input:eq(4)');
 
-    -write a get cost function to extract the cost of event as numeral using slice()
-    -write a get date/time function to exact d/t and return using search()
+function stateAndStyleEvent(checkbox){
+    return e => {
+        if($(e.target).is(':checked')){
+            $(checkbox).attr('disabled', true);
+            $(checkbox).parent().css('text-decoration', 'line-through');
+            $(checkbox).parent().css('color', 'grey');
+        }else{
+            $(checkbox).attr('disabled', false);
+            $(checkbox).parent().css('text-decoration', '');
+            $(checkbox).parent().css('color', '');
+        }
+    }
+}
+
+// event listners for individual workshop checkboxes
+$($jsFrameWorks).on('change', stateAndStyleEvent($express));
+$($jsLibs).on('change', stateAndStyleEvent($nodeJs));
+$($express).on('change', stateAndStyleEvent($jsFrameWorks));
+$($nodeJs).on('change', stateAndStyleEvent($jsLibs));
+
+
+// PAYMENT SECTION --------------------------------------------------------
+
+$('#payment option:eq(0)').remove();
+$('div p').hide();
+
+$('#payment').on('change', function(e){
+    if ($(e.target).val() === 'credit card') {
+        $('div p').hide();
+
+    }
+    else if ($(e.target).val() === 'paypal') {
+        $('div p:eq(0)').show();
+        $('div p:eq(1)').hide();
+    }
+    else {
+        $('div p:eq(1)').show();
+        $('div p:eq(0)').hide();
+    }
+})
+
+
+
+
+
+// if($(e.target).is(':checked')){
+//     $($express).attr('disabled', true);
+//     $($express).parent().css('text-decoration', 'line-through');
+//     $($express).parent().css('color', 'grey');
+// }else{
+//     $($express).attr('disabled', false);
+//     $($express).parent().css('text-decoration', '');
+//     $($express).parent().css('color', '');
+// }
+
+/*
+
+    -when the js framworks is checked, disable express
+    -when the js framworks is unchecked, enable express
+
+    -when js lib is checked, disable node.js
+    -when js lib is unchecked, enable node.js
+
+    -when express is checked, disable js frameworks
+    -when express is unchecked, enable js frameworks
+
+    -when node.js is checked, disable js lib
+    -when node.js is unchecked, enable js lib
 */
 
 
